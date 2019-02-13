508877e79f5e9e6782d6f2b75eaf75455c1c7d95
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2019-02-13 14:42:23 -0500

    Update fib.java

--- fib.java
+++ fib.java
@@ -4,30 +4,37 @@ import java.util.*;
 
 public class fib {
 	//Sets some varibles to 0 for use later
-	int x = 0;
+	int x = 1;
 	int y = 0;
 	int z = 0;
+	int sum = 0;
 	//String that contains the alphabet
 
 	
 	void fibby(){
 		System.out.println("Number!");
         Scanner console = new Scanner(System.in);
-		//prombts user for text
+
         int input = console.nextInt();
-		//converts it into lowercase
-       
-		for (int y=0; y <= input; y++) {
-			x = x+y;
-			System.out.println(x);
+
+        int[] sequence = new int[input];
+        
+		for (int y=0; y < input; y++) {
+			sum = x + z;
+			sequence [y] = x;
+			z = x;
+			x = sum;
 	
 			}
+		System.out.println("Bingo Bango you have a:");
+		System.out.println(sequence[input-1]);
 		}
 //this just runs the script
 	public static void main(String[] args) {
 		
 		fib run = new fib();
 	    run.fibby();
+	   
 		
 		
 	}	
